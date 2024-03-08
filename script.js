15347eb49fbd6aeb49505480cbbef8b4fc66045d
LohSilva <lo-ruama_sp@hotmail.com>
2024-03-08 15:53:09 -0300

    Classe, Construtor, Método

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Iniciando um novo desafio")
+//Criando a classe de heróis
+class heroe {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    //Metodo para ação de atacar
+    atacar() {
+        //Declarando variável para guardar tipo de ataque de cada herói
+        let tipoDeAtaque = ["magia", "espada", "artes marciais", "shuriken"]
+
+        if (this.tipo === "mago") {
+            console.log(`O tipo ${this.tipo} ataca usando ${tipoDeAtaque[0]}`);
+        } else if (this.tipo === "guerreiro") {
+            console.log(`O tipo ${this.tipo} ataca usando ${tipoDeAtaque[1]}`);
+        } else if (this.tipo === "monge") {
+            console.log(`O tipo ${this.tipo} ataca usando ${tipoDeAtaque[2]}`);
+        } else {
+            console.log(`O tipo ${this.tipo} ataca usando ${tipoDeAtaque[3]}`);
+        }
+
+    }
+
+}
+
+//Saída
+let ataque = new heroe("Luna", 20, "mago");
+ataque.atacar();
